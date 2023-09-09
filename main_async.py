a947bedba24291594f0bed0804f5eaa00345fe45
denis <zakablukov_77@inbox.ru>
2023-09-09 15:01:10 +0400

    Commit_09_09_2023/14:38

--- main_async.py
+++ main_async.py
@@ -40,34 +40,36 @@ async def get_value_url(urls_list, column: str):
 # И ДОБАВЛЯЕМ В БД
 async def add_person(json_value):
     async with Session() as session:
-        for character_data in json_value:
-            if character_data is not None:
+        async with ClientSession() as client_session:
+            
+            for character_data in json_value:
+                if character_data is not None:
 
-                films = await get_value_url(character_data["films"], "title"),          # --- URL
-                planets = await get_value_url(character_data["homeworld"], "name"),     # --- URL
-                species = await get_value_url(character_data["species"], "name"),       # --- URL
-                starships = await get_value_url(character_data["starships"], "name"),   # --- URL
-                vehicles = await get_value_url(character_data["vehicles"], "name"),     # --- URL
+                    films = await get_value_url(character_data["films"], "title"),          # --- URL
+                    planets = await get_value_url(character_data["homeworld"], "name"),     # --- URL
+                    species = await get_value_url(character_data["species"], "name"),       # --- URL
+                    starships = await get_value_url(character_data["starships"], "name"),   # --- URL
+                    vehicles = await get_value_url(character_data["vehicles"], "name"),     # --- URL
 
-                value_add = SwapiPerson(
-                    name=character_data["name"],
-                    birth_year=character_data["birth_year"],
-                    gender=character_data["gender"],
-                    eye_color=character_data["eye_color"],
-                    hair_color=character_data["hair_color"],
-                    height=character_data["height"],
-                    mass=character_data["mass"],
-                    skin_color=character_data["skin_color"],
-                    films=films,            # --- URL
-                    planets=planets,        # --- URL
-                    species=species,        # --- URL
-                    starships=starships,    # --- URL
-                    vehicles=vehicles,      # --- URL
-                )
+                    value_add = SwapiPerson(
+                        name=character_data["name"],
+                        birth_year=character_data["birth_year"],
+                        gender=character_data["gender"],
+                        eye_color=character_data["eye_color"],
+                        hair_color=character_data["hair_color"],
+                        height=character_data["height"],
+                        mass=character_data["mass"],
+                        skin_color=character_data["skin_color"],
+                        films=films,            # --- URL
+                        planets=planets,        # --- URL
+                        species=species,        # --- URL
+                        starships=starships,    # --- URL
+                        vehicles=vehicles,      # --- URL
+                    )
 
-                session.add(value_add)
-                print("Запись в БД выполнена")
-                await session.commit()
+                    session.add(value_add)
+                    print("Запись в БД выполнена")
+                    await session.commit()
 
 
 async def main():
